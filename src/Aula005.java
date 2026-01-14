a1425900a5b222c18ec64c9e0b1baf96b51a57b6
gbarbosa06 <andradegui2006@gmail.com>
2026-01-14 11:50:33 -0300

    Adição da aula 5

---
+++ src/Aula005.java
@@ -0,0 +1,14 @@
+public class Aula005 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        ++a;
+        a++;
+        System.out.println(a);
+    }
+//    a + b
+//    a - b
+//    a * b
+//    a / b
+//    a % b
+}
