5b480433fda3e3298155c7d667845846362414ce
gbarbosa06 <andradegui2006@gmail.com>
2026-01-22 11:30:14 -0300

    Aula 6 adicionada

---
+++ src/Aula006.java
@@ -0,0 +1,35 @@
+public class Aula006 {
+    public static void main(String[] args) {
+        double nota = 6.5;
+        int presenca = 80;
+
+        if (nota >= 7 || nota >=5 && presenca >= 75) {
+            System.out.println("Passou");
+        }
+
+        // "==" != "="
+        // 1. ! 2. && 3. ||
+        // (  )
+
+
+    }
+}
+
+/*
+Operadores Relacionais
+  x  == y
+    !=
+    >
+    <
+    >=
+    <=
+
+Operadores Lógicos
+    &&
+    ||
+    !
+
+    Resultam em:
+        true
+        false
+ */
