5e3159c9b0faf7adf747faab091c73bbb8d292b1
gbarbosa06 <andradegui2006@gmail.com>
2026-02-03 11:37:56 -0300

    Adicao aula 7

---
+++ src/Aula007.java
@@ -0,0 +1,20 @@
+public class Aula007 {
+    public static void main(String[] args) {
+        int idade = -17;
+        if(idade >= 18){
+            System.out.println("Maior de idade");
+        } else if (idade < 0) {
+                System.out.println("Numero invalido");
+            } else {
+                System.out.println("Menor de idade");
+            }
+    }
+}
+/*
+* if (condicao) {
+* codigo...
+* } else {
+* codigo...
+* }
+*
+* */
