71520de5ba6320fc5e7355d545b286fff86219f1
gbarbosa06 <andradegui2006@gmail.com>
2026-05-27 11:22:08 -0300

    aula 13

---
+++ src/aula013/Main.java
@@ -0,0 +1,11 @@
+package aula013;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Guilherme", 20);
+        System.out.println(pessoa1.nome);
+        System.out.println(pessoa1.idade);
+
+    }
+
+}
---
+++ src/aula013/Pessoa.java
@@ -0,0 +1,10 @@
+package aula013;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    Pessoa (String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
